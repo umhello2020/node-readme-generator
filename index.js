260092b6614c4d31f49fd8f1cfd762c64929df7b
Olivia Gilbert <umhello2020@gmail.com>
2023-02-12 19:37:50 -0600

    added starter code and reworked questions and .writeFile

--- index.js
+++ index.js
@@ -1,60 +1,33 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-inquirer
-    .prompt([
-        {type: "input", message: "What is the title of your project?", name: "title" },
-        {type: "input", message: "Please enter a description of your project.", name: "description "},
-        {type: "input", message: "Please enter installation instructions for your project.", name: "installation" },
-        {type: "input", message: "Please enter usage information for your project.", name: "usage" },
-        {type: "input", message: "Please enter contribution guidelines for your project.", name: "contributing" },
-        {type: "input", message: "Please enter some test instructions for your project.", name: "tests" },
-        {type: "input", message: "What is your GitHub username?", name: "username"},
-        {type: "input", message: "What is your email address?", name: "email"}
-    ]);
-    .then(function(response) {
-        console.log("response", response)
+const questions = [ 
+    {type: "input", message: "What is the title of your project?", name: "title" },
+    {type: "input", message: "Please enter a description of your project.", name: "description "},
+    {type: "input", message: "Please enter installation instructions for your project.", name: "installation" },
+    {type: "input", message: "Please enter usage information for your project.", name: "usage" },
+    {type: "list", choices: ['MIT', 'Adobe', 'none'], message: "What Markdown License have you applied to your project?", name: 'license'},
+    {type: "input", message: "Please enter contribution guidelines for your project.", name: "contributing" },
+    {type: "input", message: "Please enter some test instructions for your project.", name: "tests" },
+    {type: "input", message: "What is your GitHub username?", name: "username"},
+    {type: "input", message: "What is your email address?", name: "email"}
+];
 
-        const readMe = `
-        # ${response.title}
-        
-        ## Description
+function init() {
 
-        ${response.description}
+    inquirer
+        .prompt(questions);
+        .then((response) => {
+            writeToFile("README.md", generateMarkdown(response))
+        })
+}
 
-        ## Table of Contents
-        [Title](#-${response.title})
-        [Description](##-description)
-        [Installation Instructions](##-installation)
-        [Usage Information](##-usage)
-        [License] (##-license)
-        [Contribution Guidelines](##-contributing)
-        [Test Instructions](##-tests)
-        [Questions](##-questions)
 
-        ## Installation
-
-        ${response.installation}
-
-        ## Usage
-
-        ${response.usage}
-
-        ## License
-
-        
-
-        ## Contributing
-
-        ${response.contributing}
-
-        ## Tests
-
-        ${response.tests}
-
-        ## Questions
-        
-        
-        ${response.username}
-        `
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data, err => {
+        if (err) console.error(err);
     })
+}
+
+init();
